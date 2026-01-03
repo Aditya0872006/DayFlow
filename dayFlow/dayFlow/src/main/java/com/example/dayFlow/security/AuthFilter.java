0e2810f40f030ff40794e7b760b7bfbc37fca970
Aditya Sharma <sharmaadityamj@gmail.com>
2026-01-03 11:43:56 +0530

    jwt service and auth filter added

--- dayFlow/dayFlow/src/main/java/com/example/dayFlow/security/AuthFilter.java
+++ dayFlow/dayFlow/src/main/java/com/example/dayFlow/security/AuthFilter.java
@@ -18,10 +18,60 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
-public class AuthFilter
-{
+@Slf4j
+public class AuthFilter extends OncePerRequestFilter {
 
+    private final JwtService jwtService;
+    private final CustomUserDetailService userDetailService;
+    private final CustomAuthenticationEntryPoint entryPoint;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String token = getTokenFromRequest(request);
+
+        if (token != null) {
+            try {
+                String email = jwtService.getUsernameFromToken(token);
+                UserDetails userDetails = userDetailService.loadUserByUsername(email);
+
+                if (jwtService.isTokenValid(token, userDetails)) {
+                    UsernamePasswordAuthenticationToken auth =
+                            new UsernamePasswordAuthenticationToken(
+                                    userDetails,
+                                    null,
+                                    userDetails.getAuthorities()
+                            );
+
+                    auth.setDetails(
+                            new WebAuthenticationDetailsSource().buildDetails(request));
+
+                    SecurityContextHolder.getContext().setAuthentication(auth);
+                }
+
+            } catch (Exception ex) {
+                entryPoint.commence(
+                        request,
+                        response,
+                        new BadCredentialsException(ex.getMessage())
+                );
+                return;
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String getTokenFromRequest(HttpServletRequest request) {
+        String bearer = request.getHeader("Authorization");
+        if (bearer != null && bearer.startsWith("Bearer ")) {
+            return bearer.substring(7);
+        }
+        return null;
+    }
 }
