7bde7a4609f165958d2526b2d10d493e7975c9f4
Aditya Sharma <sharmaadityamj@gmail.com>
2026-01-03 11:52:44 +0530

    CorsConfig added

---
+++ dayFlow/dayFlow/src/main/java/com/example/dayFlow/security/CorsConfig.java
@@ -0,0 +1,30 @@
+package com.example.dayFlow.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public UrlBasedCorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.addAllowedOriginPattern("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setAllowCredentials(false); // JWT based auth
+        config.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
+}
